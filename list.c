63f486987e6ae180c2d0765f7275d8a902c1663a
18140200203-mwh <80935558+18140200203-mwh@users.noreply.github.com>
2021-03-28 15:02:58 +0800

    Update list.c

--- list.c
+++ list.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <assert.h>
-
 #include "list.h"
 
 
@@ -15,4 +14,38 @@ struct list *create_list( int init_cap )
 	l->capacity = init_cap;
     l->counter = 0; 
     return l;	
+}
+struct list* insert(struct list* l, int i, T e) {
+    int j;
+    if (l->counter == l->capacity) 
+    {
+        int new_cap = l->capacity * 2;
+        T* new_ele = (T*)malloc(sizeof(T) * new_cap);
+        for (j = 0; j < l->counter; j++) 
+        {
+            new_ele[j] = l->ele[j];
+        }
+        l->capacity = new_cap;
+        free(l->ele);
+        l->ele = new_ele;
+    }
+    assert(i >= 0 && i <= l->counter);
+    for (j = l->counter; j > i; j--) 
+    {
+        l->ele[j] = l->ele[j - 1];
+    }
+    l->counter++;
+    l->ele[i] = e;
+    return l;
+}
+struct list* delete_at(struct list* l, int i)
+{
+    int j;
+    assert(i >= 0 && i < l->counter);
+    for (j = i; j < l->counter - 1; j++) 
+    {
+        l->ele[j] = l->ele[j + 1];
+    }
+    l->counter--;
+    return l;
 }
